9635f9ae9d5d9663b98f901705b04411688a5cb2
whinstone1 <zeemurrawat@hotmail.com>
2018-02-21 13:13:31 +0500

    initial

---
+++ vidly/Controllers/MoivesController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace vidly.Controllers
+{
+    public class MoivesController : Controller
+    {
+        // GET: Moives
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ vidly/Models/movie.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace vidly.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Id { get; set; }
+        
+                
+    }
+}
